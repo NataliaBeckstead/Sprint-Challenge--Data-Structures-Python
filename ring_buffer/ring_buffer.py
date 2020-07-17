4d25b7d42ffd3ede2efb9d9eea703c61651bb6fc
Natalia Beckstead <toosmiravilla@gmail.com>
2020-07-17 10:27:03 -0600

    ring buffer tests passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.values = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        if len(self.values) < self.capacity:
+            self.values.append(item)
+        else:
+            self.values[self.index] = item
+            if self.index < len(self.values) - 1:
+                self.index += 1
+            else:
+                self.index = 0
 
     def get(self):
-        pass
+        return self.values
