5169b5350542bcecac84020ec40e12eb80267591
Natalia Beckstead <toosmiravilla@gmail.com>
2020-07-17 11:05:36 -0600

    added stretch in names with hash table

--- names/names.py
+++ names/names.py
@@ -28,9 +28,25 @@ for name_2 in names_2:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print("Binary search tree")
 print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+t_start = time.time()
+hash_duplicates = []
+
+hash_names = {names_1.pop(): True}
+for name_1 in names_1:
+    hash_names[name_1] = True
+for name_2 in names_2:
+    if name_2 in hash_names:
+        hash_duplicates.append(name_2)
+
+t_finished = time.time()
+print(f"{len(hash_duplicates)} duplicates:\n\n{', '.join(hash_duplicates)}\n\n")
+print("hash table")
+print(f"runtime: {t_finished - t_start} seconds")
